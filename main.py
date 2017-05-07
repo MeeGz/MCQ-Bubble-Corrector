4a9cf31f1695e33538e7410f8340d9e65a51bdd4
MeeGz <mohamed_magdy1696@hotmail.com>
2017-05-07 16:40:11 +0200

    multiple choices > 2 handled

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ model_answers = {1: "B", 2: "C", 3: "A", 4: "A", 5: "D", 6: "A", 7: "C", 8: "C",
                  41: "B", 42: "B", 43: "C", 44: "C", 45: "B"}
 total_grade = 0
 faults = 0
-dir_path = "/home/meegz/Projects/Image Processing/Dataset/tests/"
+dir_path = "/home/meegz/Projects/Image Processing/Dataset/tasky/"
 write_list = []
 toWrite = []
 wrong_detection_count = 0
